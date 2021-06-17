6f23fbf250ad03e2ef5b9e06adb7819974aeda49
SbCoiner <80302821+SbCoiner@users.noreply.github.com>
2021-06-16 19:24:08 -0700

    Made the program exit after completion

--- install.py
+++ install.py
@@ -1,8 +1,6 @@
 import os
 import platform
 if platform.system() == "Windows":
-    os.system('cmd /k "py -m pip install PyQt5 & py -m pip install keyboard"')
-if platform.system() == "Linux":
-    os.system('pip install PyQt5 ; pip install keyboard')
-if platform.system() == "Darwin":
-    os.system('pip install PyQt5 ; pip install keyboard')
+    os.system('cmd /k "py -m pip install PyQt5 & py -m pip install keyboard && exit"')
+if platform.system() == "Linux" or platform.system() == "Darwin":
+    os.system('pip install PyQt5 ; pip install keyboard ; exit')
